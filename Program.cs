6de8f12726a38306e77a3238563bb5076b5485f4
byLimeOn <30927853+byLimeOn@users.noreply.github.com>
2022-07-13 21:37:42 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Здрав будь, иноземец! Дабы проследовать в покои царские на вопросы ответь сперва");
+        Console.WriteLine("Как величать тебя?");
+        string name = Console.ReadLine();
+        Console.WriteLine("Откуда путь держишь?");
+        string city = Console.ReadLine();
+        Console.WriteLine("Лет от роду как много?");
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Иноземец " + name + " из места, именуемого " + city + ", лет от роду прожив " + age + ", почтил нас своим визитом!");
+    }
+}
