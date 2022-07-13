0ac4729e18189c4c4c99ac5eb8b44fb4f8220cb2
byLimeOn <30927853+byLimeOn@users.noreply.github.com>
2022-07-13 22:24:02 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "Петров";
+        string surname = "Степан";
+        Console.WriteLine("Данные до перестановки:\nИмя: "+name+"\nФамилия: "+surname);
+        string transposition = name;
+        name = surname;
+        surname = transposition;
+        Console.WriteLine("Данные после перестановки:\nИмя: " + name + "\nФамилия: " + surname);
+    }
+}
