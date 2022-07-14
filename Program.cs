6d22f344743e93c4dc38552940b8fcabc3e4d3b0
byLimeOn <30927853+byLimeOn@users.noreply.github.com>
2022-07-14 20:10:17 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int pictures = 52;
+        int picturesInRow = 3;
+        int fullRows = pictures / picturesInRow;
+        int remain = pictures % picturesInRow;
+        Console.WriteLine($"Из {pictures} картинок по {picturesInRow} в ряд получилось {fullRows} полных рядов. {remain} картинок вышло сверх меры");
+    }
+}
