1a42a20a52e68d5b9a0b8624b699c2229888d378
byLimeOn <30927853+byLimeOn@users.noreply.github.com>
2022-07-14 21:15:38 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int onePeopleTime = 10;
+        Console.Write("Войдя в поликлинику? Вы видите толпу старушек. Окинув взглядом, насчиталось ");
+        int allPeople = Convert.ToInt32(Console.ReadLine());
+        int inTurnTime = allPeople * onePeopleTime;
+        int hours = inTurnTime / 60;
+        int minutes = inTurnTime % 60;
+        Console.WriteLine("Время ожидания составит " + hours + " часа " + minutes + " минут.");
+    }
+}
