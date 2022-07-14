4c79343ee155cd80b00639ef036d46080f8d6727
byLimeOn <30927853+byLimeOn@users.noreply.github.com>
2022-07-14 20:39:15 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int allCrystals = 100;
+        int costOfCrystals = 12;
+        Console.WriteLine("Сколько у Вас золотишка?");
+        int gold = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Сколько кристаллов нужно?");
+        int needCrystals = Convert.ToInt32(Console.ReadLine());
+        int price = needCrystals * costOfCrystals;
+        Console.WriteLine("Ваши " + price + $" золота за {needCrystals} кристаллов. Сдача " + (gold - price) + " золота. Ваш чек пожалуйста. У нас осталось ещё " + (allCrystals - needCrystals) + " кристаллов");
+    }
+}
