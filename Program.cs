5fdad8d9a45785353883e8e5bb6ec2afd95b541c
byLimeOn <30927853+byLimeOn@users.noreply.github.com>
2022-07-14 21:04:56 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 class Program
 {
     static void Main(string[] args)
@@ -10,6 +10,8 @@ class Program
         Console.WriteLine("Сколько кристаллов нужно?");
         int needCrystals = Convert.ToInt32(Console.ReadLine());
         int price = needCrystals * costOfCrystals;
-        Console.WriteLine("Ваши " + price + $" золота за {needCrystals} кристаллов. Сдача " + (gold - price) + " золота. Ваш чек пожалуйста. У нас осталось ещё " + (allCrystals - needCrystals) + " кристаллов");
+        int oddGold = gold - price;
+        int oddCrystals = allCrystals - needCrystals;
+        Console.WriteLine("Ваши " + price + $" золота за {needCrystals} кристаллов. Сдача " + oddGold + " золота. Ваш чек пожалуйста. У нас осталось ещё " + oddCrystals + " кристаллов");
     }
 }
